4cdf9f8ba643f6af64382e99a2c51e2f65fed464
Murilo Avila <murilo.avila068@gmail.com>
2023-08-31 15:28:46 -0300

    feat: addAccount UseCase

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,31 +1,44 @@
-import { MissingParamError, InvalidParamError } from "../errors/";
-import { badRequest, serverError } from "../helpers/http-helper";
-import { HttpRequest, HttpResponse, EmailValidator, Controller } from "../protocols";
+import { AddAccount } from '../../domain/usecases/add-account'
+import { MissingParamError, InvalidParamError } from '../errors/'
+import { badRequest, serverError } from '../helpers/http-helper'
+import { HttpRequest, HttpResponse, EmailValidator, Controller } from '../protocols'
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount) {}
 
-  handle(httpRequest: HttpRequest): HttpResponse {
+  handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = [
-        "name",
-        "email",
-        "password",
-        "passwordConfirmation",
-      ];
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
 
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
+          return badRequest(new MissingParamError(field))
         }
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email);
-      if (!isValid) {
-        return badRequest(new InvalidParamError("email"));
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      this.addAccount.add({ name, email, password })
+
+      return { statusCode: 201, body: {} }
     } catch (error) {
-      return serverError();
+      return serverError()
     }
   }
 }
