5366081fb6712bd4887e6ba7872a811d16b5820c
Murilo Avila <murilo.avila068@gmail.com>
2023-08-31 11:21:23 -0300

    feat: initial Commit

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,33 @@
+import { MissingParamError, InvalidParamError } from "../errors/";
+import { badRequest, serverError } from "../helpers/http-helper";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/email-validator";
+
+export class SignUpController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
+  handle(httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFields = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
