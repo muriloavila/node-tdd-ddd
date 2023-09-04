be554ad342c0634b41c026d9f4e2d2efeb547c8d
Murilo Avila <murilo.avila068@gmail.com>
2023-09-04 17:07:19 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "jest";
+import type { Config } from 'jest'
 
 const config: Config = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest",
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
 
-export default config;
+export default config
