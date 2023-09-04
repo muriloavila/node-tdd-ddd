1bece6a9365edc29b9fb7eba5e0689f5067d49b3
Murilo Avila <murilo.avila068@gmail.com>
2023-09-04 14:58:34 -0300

    refactor: lint all the files in the project

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
-  constructor(paramName: string) {
-    super(`Missing param: ${paramName}`);
-    this.name = "MissingPramError";
+  constructor (paramName: string) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingPramError'
   }
 }
