e267f3f4399372233f1f0960a149e60fa09c6ad5
Murilo Avila <murilo.avila068@gmail.com>
2023-09-04 17:24:03 -0300

    test: ensure emailValidatorAdapter calls with correct values

--- src/utils/email-validator-adaptor.spec.ts
+++ src/utils/email-validator-adaptor.spec.ts
@@ -24,4 +24,12 @@ describe('EmailValidator Adapter', () => {
 
     expect(isValid).toBe(true)
   })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isEmailSPy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSPy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
